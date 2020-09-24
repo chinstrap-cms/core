1d466189a649b310eb973425bc0c7d88d7e1e061
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-24 21:11:36 +0100

    Fixed broken tests

--- tests/Unit/Kernel/KernelTest.php
+++ tests/Unit/Kernel/KernelTest.php
@@ -24,37 +24,9 @@ final class KernelTest extends TestCase
         parent::tearDown();
     }
 
-    public function testErrorHandler()
-    {
-        $handler = m::mock('Chinstrap\Core\Contracts\Exceptions\Handler');
-        $router = m::mock('League\Route\Router');
-        $router->shouldReceive('get')
-            ->andReturn($router);
-        $router->shouldReceive('middleware')
-            ->andReturn($router);
-        $router->shouldReceive('post')
-            ->andReturn($router);
-        $container = m::mock('League\Container\Container');
-        $container->shouldReceive('delegate')->once();
-        $container->shouldReceive('addServiceProvider');
-        $container->shouldReceive('share')->times(2);
-        $container->shouldReceive('get')->with('League\Route\Router')
-            ->once()
-            ->andReturn($router);
-        $container->shouldReceive('get')->with('Chinstrap\Core\Contracts\Exceptions\Handler')
-            ->once()
-            ->andReturn($handler);
-        $container->shouldReceive('get')->with('PublishingKit\Config\Config')
-            ->once()
-            ->andReturn(new Config([]));
-        $app = new Kernel($container);
-        $app->bootstrap();
-    }
-
     public function testPluginNotFound()
     {
         $this->expectException('Chinstrap\Core\Exceptions\Plugins\PluginNotFound');
-        $handler = m::mock('Chinstrap\Core\Contracts\Exceptions\Handler');
         $container = m::mock('League\Container\Container');
         $container->shouldReceive('delegate')->once();
         $container->shouldReceive('addServiceProvider');
@@ -62,9 +34,6 @@ final class KernelTest extends TestCase
         $container->shouldReceive('get')->with('My\Nonexistent\Plugin')
             ->once()
             ->andReturn(null);
-        $container->shouldReceive('get')->with('Chinstrap\Core\Contracts\Exceptions\Handler')
-            ->once()
-            ->andReturn($handler);
         $config = new Config([
                               'plugins' => ['My\Nonexistent\Plugin'],
                              ]);
@@ -78,7 +47,6 @@ final class KernelTest extends TestCase
     public function testPluginNotValid()
     {
         $this->expectException('Chinstrap\Core\Exceptions\Plugins\PluginNotValid');
-        $handler = m::mock('Chinstrap\Core\Contracts\Exceptions\Handler');
         $container = m::mock('League\Container\Container');
         $container->shouldReceive('delegate')->once();
         $container->shouldReceive('addServiceProvider');
@@ -86,9 +54,6 @@ final class KernelTest extends TestCase
         $container->shouldReceive('get')->with('stdClass')
             ->once()
             ->andReturn(new \stdClass());
-        $container->shouldReceive('get')->with('Chinstrap\Core\Contracts\Exceptions\Handler')
-            ->once()
-            ->andReturn($handler);
         $config = new Config([
                               'plugins' => ['stdClass'],
                              ]);
