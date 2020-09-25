5f68c7565a745de4b0f5036f7f7b484ae41c8990
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-25 12:09:54 +0100

    Added app factory class

---
+++ src/Kernel/AppFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Kernel;
+
+use Chinstrap\Core\Http\Middleware\ClockworkMiddleware;
+use Chinstrap\Core\Http\Middleware\HttpCachingProxyMiddleware;
+use Chinstrap\Core\Http\Middleware\NotFoundMiddleware;
+use Chinstrap\Core\Http\Middleware\RoutesMiddleware;
+use Chinstrap\Core\Http\Middleware\WhoopsMiddleware;
+use Laminas\Stratigility\MiddlewarePipe;
+
+final class AppFactory
+{
+    public function __invoke(): MiddlewarePipe
+    {
+        $app = new MiddlewarePipe();
+
+        $kernel = new Kernel();
+        $kernel->bootstrap();
+        $container = $kernel->getContainer();
+
+        $app->pipe($container->get(WhoopsMiddleware::class));
+        $app->pipe($container->get(ClockworkMiddleware::class));
+        $app->pipe($container->get(HttpCachingProxyMiddleware::class));
+        $app->pipe($container->get(RoutesMiddleware::class));
+        $app->pipe($container->get(NotFoundMiddleware::class));
+        return $app;
+    }
+}
