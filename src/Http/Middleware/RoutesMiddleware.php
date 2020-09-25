bd15cc95a56afdff7b4c63ee1712ace617ef68a8
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-25 10:10:57 +0100

    Added events for registering static and dynamic routes
    These will be used to register routes so that static ones can be
    registered first without risking shadowing dynamic ones

--- src/Http/Middleware/RoutesMiddleware.php
+++ src/Http/Middleware/RoutesMiddleware.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Chinstrap\Core\Http\Middleware;
 
+use Chinstrap\Core\Events\RegisterDynamicRoutes;
+use Chinstrap\Core\Events\RegisterStaticRoutes;
+use League\Event\EmitterInterface;
 use League\Route\Http\Exception\NotFoundException;
 use League\Route\Router;
 use Psr\Http\Message\ServerRequestInterface;
@@ -18,13 +21,16 @@ final class RoutesMiddleware implements MiddlewareInterface
      */
     private $router;
 
-    public function __construct(Router $router)
+    public function __construct(Router $router, EmitterInterface $emitter)
     {
         $this->router = $router;
+        $this->emitter = $emitter;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $this->emitter->emit(new RegisterStaticRoutes());
+        $this->emitter->emit(new RegisterDynamicRoutes());
         try {
             return $this->router->dispatch($request);
         } catch (NotFoundException $e) {
