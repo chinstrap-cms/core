13438e0bc3e1f88d04f597dc5bd2f20e5a430ea4
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-25 10:20:33 +0100

    Created listeners

---
+++ src/Listeners/RegisterAllDynamicRoutes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Listeners;
+
+final class RegisterAllDynamicRoutes extends BaseListener
+{
+}
---
+++ src/Listeners/RegisterAllStaticRoutes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Listeners;
+
+final class RegisterAllStaticRoutes extends BaseListener
+{
+}
