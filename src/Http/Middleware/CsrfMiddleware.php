d554d4c02e8071321bb6f445e7b94c8df6b3216e
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-28 19:55:36 +0100

    Removing CSRF middleware
    Doesn't really work as hoped so just going to keep the Laminas Session
    implementation.

--- src/Http/Middleware/CsrfMiddleware.php
+++ src/Http/Middleware/CsrfMiddleware.php
@@ -9,9 +9,21 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use PublishingKit\Csrf\Token;
+use PublishingKit\Csrf\TokenStorage;
 
 final class CsrfMiddleware implements MiddlewareInterface
 {
+    /**
+     * @var TokenStorage
+     */
+    private $storage;
+
+    public function __construct(TokenStorage $storage)
+    {
+        $this->storage = $storage;
+        $this->key = 'foo';
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -20,8 +32,11 @@ final class CsrfMiddleware implements MiddlewareInterface
         $method = $request->getMethod();
         if (in_array($method, ['GET', 'HEAD', 'OPTIONS'])) {
             $response = $handler->handle($request);
-            return $response->withAddedHeader('X-CSRF-TOKEN', Token::generate()->__toString());
+            $token = Token::generate();
+            $this->storage->store($this->key, $token);
+            return $response->withAddedHeader('X-CSRF-TOKEN', $token->__toString());
         } else {
+            $token = $this->storage->retrieve($this->key);
             return $handler->handle($request);
         }
     }
