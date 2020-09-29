d51d3039d0a6740558bae474310fdcadab1cd410
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-29 14:33:18 +0100

    Added alias for renderer

--- src/Kernel/ContainerFactory.php
+++ src/Kernel/ContainerFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Chinstrap\Core\Kernel;
 
+use Chinstrap\Core\Contracts\Views\Renderer;
+use Chinstrap\Core\Views\TwigRenderer;
 use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 use Laminas\ServiceManager\ServiceManager;
 
@@ -12,7 +14,12 @@ final class ContainerFactory
     public function __invoke(): ServiceManager
     {
         return new ServiceManager([
-            'abstract_factories' => ReflectionBasedAbstractFactory::class,
+            'abstract_factories' => [
+                ReflectionBasedAbstractFactory::class,
+            ],
+            'aliases' => [
+                Renderer::class => TwigRenderer::class,
+            ]
         ]);
     }
 }
