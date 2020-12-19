e1f72dc691b0353998f73dcefdb22287e7e6e527
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-12-19 20:37:07 +0000

    Updated Mix filter to support hot loading

--- src/Views/Filters/Mix.php
+++ src/Views/Filters/Mix.php
@@ -10,7 +10,7 @@ final class Mix
 {
     public function __invoke(string $path): string
     {
-        $manifest = json_decode(file_get_contents(PUBLIC_DIR . DIRECTORY_SEPARATOR . 'mix-manifest.json'), true);
+        $manifest = json_decode(rtrim(file_get_contents(PUBLIC_DIR . DIRECTORY_SEPARATOR . 'mix-manifest.json')), true);
         if (! array_key_exists("/" . $path, $manifest)) {
             throw new Exception(
                 "Unable to locate Mix file: {$path}"
@@ -19,6 +19,27 @@ final class Mix
         if (!file_exists(PUBLIC_DIR . DIRECTORY_SEPARATOR . $path)) {
             throw new Exception('Included file does not exist');
         }
+
+        if (file_exists(PUBLIC_DIR . DIRECTORY_SEPARATOR . '/hot')) {
+            $url = rtrim(file_get_contents(PUBLIC_DIR . DIRECTORY_SEPARATOR . '/hot'));
+
+            if ($this->startsWith($url, 'http://') || $this->startsWith($url, 'https://')) {
+                return $this->after($url, ':') . $path;
+            }
+
+            return "//localhost:8080{$path}";
+        }
         return $manifest["/" . $path];
+    }
+
+    private function startsWith(string $text, string $start): bool
+    {
+          $len = strlen($start);
+          return (substr($text, 0, $len) === $start);
+    }
+
+    private function after(string $text, string $separator): string
+    {
+        return substr($text, strpos($text, $separator) + 1);
     }
 }
