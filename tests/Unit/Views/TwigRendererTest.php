bae10ad95e5f5556dd5d2615d20f6dd724b011f2
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-12-20 21:03:17 +0000

    Fixed Twig renderer test

--- tests/Unit/Views/TwigRendererTest.php
+++ tests/Unit/Views/TwigRendererTest.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace Chinstrap\Core\Tests\Unit\Views;
 
 use Chinstrap\Core\Tests\TestCase;
-use Mockery as m;
 use Chinstrap\Core\Views\TwigRenderer;
+use Mockery as m;
+use Twig\TemplateWrapper;
 
 final class TwigRendererTest extends TestCase
 {
     public function testRenderer(): void
     {
-        $this->markTestIncomplete();
         $twig = m::mock('Twig\Environment');
-        $twig->shouldReceive('load')->with('foo.html')->once()->andReturn($twig);
-        $twig->shouldReceive('render')->with(['Foo'])->once()->andReturn(['Foo']);
+        $tmpl = m::mock('Twig\Template');
+        $tmpl->shouldReceive('render')->once()->andReturn('Foo');
+        $wrapper = new TemplateWrapper($twig, $tmpl);
+        $twig->shouldReceive('load')->with('foo.html')->once()->andReturn($wrapper);
         $response = m::mock('Psr\Http\Message\ResponseInterface');
         $response->shouldReceive('getBody')->once()->andReturn($response);
         $response->shouldReceive('write')->once()->andReturn($response);
