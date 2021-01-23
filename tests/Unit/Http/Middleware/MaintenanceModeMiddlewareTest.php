5dc89b17724d530c4d4243328e37e3bc9d2f743f
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-01-23 18:57:33 +0000

    Added a test for the maintenance mode middleware

---
+++ tests/Unit/Http/Middleware/MaintenanceModeMiddlewareTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Tests\Unit\Http\Middleware;
+
+use Chinstrap\Core\Http\Middleware\MaintenanceModeMiddleware;
+use Chinstrap\Core\Tests\TestCase;
+use Mockery as m;
+use Psr\Http\Message\ResponseInterface;
+
+final class MaintenanceModeMiddlewareTest extends TestCase
+{
+    public function testActive()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getServerParams')
+                ->once()
+                ->andReturn([
+                    'MAINTENANCE' => 600
+                ]);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('withStatus')->with(503)->andReturn($response);
+        $response->shouldReceive('withAddedHeader')->with('Retry-After', 600)->andReturn($response);
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldNotReceive('handle');
+        $renderer = m::mock('Chinstrap\Core\Contracts\Views\Renderer');
+        $renderer->shouldReceive('render')->with($response, 'maintenance.html');
+        $middleware = new MaintenanceModeMiddleware($renderer, $response);
+        $received = $middleware->process($request, $handler);
+        $this->assertInstanceOf(ResponseInterface::class, $received);
+    }
+
+    public function testInactive()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getServerParams')
+                ->once()
+                ->andReturn([
+                ]);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')
+            ->andReturn($response);
+        $renderer = m::mock('Chinstrap\Core\Contracts\Views\Renderer');
+        $middleware = new MaintenanceModeMiddleware($renderer, $response);
+        $received = $middleware->process($request, $handler);
+        $this->assertSame($response, $received);
+    }
+}
