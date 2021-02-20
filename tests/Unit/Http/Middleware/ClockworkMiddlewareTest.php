cb15227d4b7b986ca93f1c56adda642f053d2cea
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-02-20 16:05:24 +0000

    Added test for Clockwork middleware

---
+++ tests/Unit/Http/Middleware/ClockworkMiddlewareTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Tests\Unit\Http\Middleware;
+
+use Chinstrap\Core\Http\Middleware\ClockworkMiddleware;
+use Chinstrap\Core\Tests\TestCase;
+use Clockwork\Support\Vanilla\Clockwork;
+use Mockery as m;
+
+final class ClockworkMiddlewareTest extends TestCase
+{
+    public function testProcessInDev(): void
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getServerParams')
+                ->once()
+                ->andReturn([
+                    'APP_ENV' => 'development'
+                ]);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')->with($request)->andReturn($response);
+        $clockwork = m::mock(Clockwork::class);
+        $clockwork->shouldReceive('usePsrMessage')
+                  ->with($request, $response)
+                  ->once()
+                  ->andReturn($clockwork);
+        $clockwork->shouldReceive('requestProcessed')->once()
+            ->andReturn($response);
+        $middleware = new ClockworkMiddleware($clockwork);
+        $received = $middleware->process($request, $handler);
+        $this->assertSame($response, $received);
+    }
+
+    public function testProcessInProduction(): void
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getServerParams')
+                ->once()
+                ->andReturn([
+                    'APP_ENV' => 'production'
+                ]);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')->with($request)->andReturn($response);
+        $clockwork = m::mock(Clockwork::class);
+        $middleware = new ClockworkMiddleware($clockwork);
+        $received = $middleware->process($request, $handler);
+        $this->assertSame($response, $received);
+    }
+}
