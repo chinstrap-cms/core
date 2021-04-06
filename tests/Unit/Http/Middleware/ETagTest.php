c453227839a43dcd6e31de7f692edc7291929b84
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-04-06 15:41:48 +0100

    Fixed a coverage gap

--- tests/Unit/Http/Middleware/ETagTest.php
+++ tests/Unit/Http/Middleware/ETagTest.php
@@ -51,4 +51,20 @@ final class ETagTest extends TestCase
         $received = $middleware->process($request, $handler);
         $this->assertEquals($received, $response);
     }
+
+    public function testGetWithoutExistingEtag()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getMethod')->andReturn('GET');
+        $request->shouldReceive('getServerParams')->andReturn(['APP_ENV' => 'production']);
+        $request->shouldReceive('getHeader')->with('if-none-match')->andReturn(null);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody->getContents')->andReturn('foo');
+        $response->shouldReceive('withAddedHeader')->andReturn($response);
+        $response->shouldReceive('withStatus')->with(304)->andReturn($response);
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')->with($request)->andReturn($response);
+        $middleware = new ETagMiddleware();
+        $received = $middleware->process($request, $handler);
+    }
 }
