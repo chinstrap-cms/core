73e6c7c67b4de348ca80ae4ab361ef1a9afb37d3
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-04-10 22:54:04 +0100

    Fixed more Psalm issues

--- src/Sources/Decorators/Psr6CacheDecorator.php
+++ src/Sources/Decorators/Psr6CacheDecorator.php
@@ -6,6 +6,7 @@ namespace Chinstrap\Core\Sources\Decorators;
 
 use Chinstrap\Core\Contracts\Objects\Document;
 use Chinstrap\Core\Contracts\Sources\Source;
+use Exception;
 use Psr\Cache\CacheItemPoolInterface;
 use PublishingKit\Utilities\Contracts\Collectable;
 
@@ -36,7 +37,11 @@ final class Psr6CacheDecorator implements Source
     {
         $item = $this->cache->getItem('Documents/find/' . $name);
         if ($item->isHit()) {
-            return $item->get();
+            $result = $item->get();
+            if (!$result instanceof Document) {
+                throw new Exception('Non-document returned from cache');
+            }
+            return $result;
         }
         $result = $this->source->find($name);
         $item->set($result);
