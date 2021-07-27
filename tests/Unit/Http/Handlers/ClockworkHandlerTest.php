508d6003ad83ec08f53f833d49f599186050a312
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-07-27 12:39:04 +0100

    Fixed more broken tests

--- tests/Unit/Http/Handlers/ClockworkHandlerTest.php
+++ tests/Unit/Http/Handlers/ClockworkHandlerTest.php
@@ -6,33 +6,16 @@ namespace Chinstrap\Core\Tests\Unit\Http\Handlers;
 
 use Chinstrap\Core\Http\Handlers\ClockworkHandler;
 use Chinstrap\Core\Tests\TestCase;
+use Clockwork\Support\Vanilla\Clockwork;
 use Mockery as m;
 
 final class ClockworkHandlerTest extends TestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-        if (!defined('E_STRICT')) {
-            return;
-        }
-
-        error_reporting('E_ALL | E_STRICT');
-    }
-
-    public function tearDown(): void
-    {
-        error_reporting(E_ALL);
-        parent::tearDown();
-    }
-
     public function testGetResponse(): void
     {
-        $clockwork = m::mock('Clockwork\Support\Vanilla\Clockwork')->makePartial();
-        $clockwork->shouldReceive('getMetadata')
-            ->with('foo')
-            ->once()
-            ->andReturn(['bar' => 'baz']);
+        $clockwork = $this->createStub(Clockwork::class);
+        $clockwork->method('getMetadata')
+            ->willReturn(['bar' => 'baz']);
         $handler = new ClockworkHandler($clockwork);
         $request = m::mock('Psr\Http\Message\ServerRequestInterface');
         $response = $handler($request, ['request' => 'foo']);
